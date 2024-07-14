8f1817052ffc37ac168cfc20a4085f6a88a31a7e
mecls <carvalhal.busin@gmail.com>
2024-07-14 22:33:46 +0100

    adding first component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,32 +1,39 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, Image } from 'react-native';
+import Colors from '@/constants/Colors';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 import React from 'react';
+import products from '@/assets/data/products';
+
+
+const product = products[0];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Image source={{uri: product.image}} style={styles.image}/>
+   <Text style={styles.title}>{product.name}</Text>
+    <Text style={styles.price}>€{product.price}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+   backgroundColor: 'white',
+   padding: 10,
+   borderRadius: 20
   },
   title: {
     fontSize: 20,
+    fontWeight: '600',
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint, 
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  image: {
+    width:'100%',
+    aspectRatio: 1  }
 });
